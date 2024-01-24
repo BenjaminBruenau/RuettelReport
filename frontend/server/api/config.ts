9c0eb1e762db5441adf872ab89e09219138b0ca5
Benni <benjaminbruenau@gmail.com>
2024-01-24 10:07:42 +0100

    trigger release

--- frontend/server/api/config.ts
+++ frontend/server/api/config.ts
@@ -1,7 +1,7 @@
 
 export const fusionAuthConfig = {
     apiKey: 'FbX31ng685J3e3Fcy4xWaDcDPUg-PMwgyin_RVHGPLnUKbXuG3ZxuUVT',
-    baseURL: 'http://10.48.8.215',
+    baseURL: 'http://my-fusion-fusionauth.default.svc.cluster.local:9111',
 
     defaultApplicationId: '9643e3b2-9f1e-4f21-8457-7d631bfde25e',
 
